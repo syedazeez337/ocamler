17c80e13eca485d74c4540780a13624f0761000e
Syed Azeez <syedazeez337@gmail.com>
2025-02-04 18:45:06 +0530

    chapter four from little schemer book

---
+++ 04_numbers/game.ml
@@ -0,0 +1,30 @@
+(*
+Key Concepts:
+Recursive Arithmetic : Solving arithmetic problems by breaking them into smaller subproblems.
+Base Case : The simplest case that can be solved directly (e.g., 0).
+Recursive Case : Breaking the problem into smaller subproblems and combining the results.
+*)
+
+(* add function using recursion *)
+let rec add m n =
+  match n with
+  | 0 -> m
+  | _ -> add (m + 1) (n - 1)
+
+(* multiply function using recursion *)
+let rec multiply m n =
+  match n with
+  | 0 -> m
+  | _ -> add m (multiply m (n - 1))
+
+(* exponentiation function raises a number to a power recursively *)
+let rec expo m n =
+  match n with
+  | 0 -> 1
+  | _ -> multiply m (expo m (n - 1))
+
+(* Subtraction function *)
+let rec subtract m n =
+  match n with
+  | 0 -> m
+  | _ -> subtract (m - 1) (n - 1)
